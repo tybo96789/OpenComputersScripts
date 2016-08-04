efa571c3894b665868c369668b70a6ee15ccc5a2
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2016-08-03 15:59:02 -1000

    Added Override feature
    Added a override feature to restore power if the user requests so and
    wait a specified amount of time to check power status again

--- power.lua
+++ power.lua
@@ -1,6 +1,7 @@
 --**********
 --Simple program to send a redstone signal when power is below and above a certain percentage
 --Created by tybo96789
+--Version 1.1
 --**********
 
 
@@ -8,6 +9,7 @@
 local component = require("component")
 local sides = require("sides")
 local os = require("os")
+local keyboard = require("keyboard")
 
 --Component proxies
 local cell = component.proxy("")
@@ -17,25 +19,41 @@ local rs = component.proxy("")
 local status = false
 local offVal = 25
 local onVal = 50
+local overrideTimer = 300
+
 --Must use sides enum
 local rsSide
 local updateInterval = 30
 
 --Code
 while true do
+--Print the current percentage of power remaining in the cell
 local pwrlvl = cell.getEnergyStored()/cell.getMaxEnergyStored()*100
-print(pwrlvl)
+print(pwrlvl .. %%)
 
+--If user is holding down the 'Control' key and it is in lower power mode, restore power and recheck status again for the specified wait period 
+if (keyboard.isControlDown() and status == true) then
+rs.setOutput(rsSide,0)
+print("Override Requested! Resuming Automation")
+print("System will recheck status in " .. overrideTimer .. " seconds")
+os.sleep(overrideTimer)
+pwrlvl = cell.getEnergyStored()/cell.getMaxEnergyStored()*100
+print(pwrlvl .. %%)
+end
+
+--If power percentage is below the specified off value, send redstone signal
 if (pwrlvl < offVal and status == false) then
 rs.setOutput(rsSide,15)
 print("Power Levels Critical! Pasuing Automation!")
 status = true
 end
 
+--If power percentages is above the specified on value, do not continue to send redstone signal
 if(pwrlvl > onVal and status == true) then
 rs.setOutput(rsSide,0)
 print("Power Levels Normal! Resuming Automation!")
 status = false
 end
+--Sleep the program for the specified amount of time
 os.sleep(updateInterval)
 end
