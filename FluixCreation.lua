0bb763a1f895cd60c302e606e6f80cc342c7eae7
Tyler A <tybo96789@users.noreply.github.com>
2016-08-04 14:58:29 -1000

    Fixed typo in code and make program run a loop
    Fixed typo and made program run in a loop

--- FluixCreation.lua
+++ FluixCreation.lua
@@ -1,6 +1,9 @@
 --************
 --Simple program that creates Fluix Crystals
+--Assumes that required items are in a chest front of the robot and water pool is 1 block behind them
+--Returns finished product back to the orignal chest that the robot took the items from
 --Created by tybo96789
+--Version: 1.01
 --************
 
 --Required Dependences
@@ -13,9 +16,11 @@ local robot = require("robot")
 local inv = component.proxy(component.inventory_controller.address)
 
 --Code
+print("Use \"CONTROL + ALT + C\" to terminate program")
+
 while true do
 
-if (inv.getSlotStackSize(sides.front,1) == 1 and inv.getSlotStackSize(sides.front,2) == 1, and inv.getSlotStackSize(sides.front,3) == 1) then
+if (inv.getSlotStackSize(sides.front,1) == 1 and (inv.getSlotStackSize(sides.front,2) == 1 and inv.getSlotStackSize(sides.front,3) == 1) then
 
 inv.suckFromSlot(sides.front,1,1)
 inv.suckFromSlot(sides.front,2,1)
