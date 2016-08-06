4b850aa30291dab08b4f9d65d44709a22a091de3
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2016-08-05 16:24:59 -1000

    Comments/ Component checks
    Added comments to the code and added component checks to see if the
    required components are accessible to the computer

--- FluixCreation.lua
+++ FluixCreation.lua
@@ -3,7 +3,7 @@
 --Assumes that required items are in a chest front of the robot and water pool is 1 block behind them
 --Returns finished product back to the orignal chest that the robot took the items from
 --Created by tybo96789
---Version: 1.01
+--Version: 1.02
 --************
 
 --Required Dependences
@@ -18,10 +18,17 @@ local inv = component.proxy(component.inventory_controller.address)
 --Code
 print("Use \"CONTROL + ALT + C\" to terminate program")
 
+if not component.isAvailable("inventory_controller") then
+print("A Inventory Controller is required!)
+os.exit()
+end
+
 while true do
 
+--Check if the inventory has 1 of the following Charged Certus Quartz, Redstone Dust, Nether Quartz
 if (inv.getSlotStackSize(sides.front,1) == 1 and (inv.getSlotStackSize(sides.front,2) == 1 and inv.getSlotStackSize(sides.front,3) == 1) then
 
+--Grab those items and store in robot internal storage
 inv.suckFromSlot(sides.front,1,1)
 inv.suckFromSlot(sides.front,2,1)
 inv.suckFromSlot(sides.front,3,1)
@@ -31,6 +38,7 @@ robot.turnLeft()
 
 robot.forward()
 
+--Drop the items into the pool of water
 robot.select(3)
 robot.dropDown()
 
@@ -40,6 +48,7 @@ robot.dropDown()
 robot.select(1)
 robot.dropDown()
 
+--Wait for items to craft before sucking up the finished product
 os.sleep(10)
 
 robot.suckDown()
